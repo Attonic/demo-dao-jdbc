cd24cb47c12018ec4d3a5c127efea4bd08675e3c
attonic <a.cleisonn@gmail.com>
2025-05-09 10:43:27 -0300

    Add: Implemeta o metodo de busca por id de departamento em SellerDaoJDBC

--- src/application/program.java
+++ src/application/program.java
@@ -6,17 +6,26 @@ import model.entities.Department;
 import model.entities.Seller;
 
 import java.util.Date;
+import java.util.List;
 
 public class program {
     public static void main(String[] args) {
 
         SellerDao sellerDao = DaoFactory.creatSellerDao();
 
-        System.out.println("=== test 1: seller findbyId: ");
+        System.out.println("==== test 1: seller findbyId: ====");
         Seller seller  = sellerDao.findById(3);
 
         System.out.println(seller);
 
+
+        System.out.println("\n==== TEST 2: SEller findByDepartment ====");
+        Department department = new Department(2, null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+
+        for(Seller seller1: list){
+            System.out.println(seller1);
+        }
 
         /*
         Department department = new Department(1, "Books");
